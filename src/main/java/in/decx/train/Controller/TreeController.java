2b575610766453a0c96da3d31ff0425f2bd67179
rohit <rohitron0609@gmail.com>
2024-03-02 20:56:31 +0530

    Fixed issues with index.html

--- src/main/java/in/decx/train/Controller/TreeController.java
+++ src/main/java/in/decx/train/Controller/TreeController.java
@@ -4,17 +4,18 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
 @RestController
-@Controller
 public class TreeController {
 
     @GetMapping("/")
-    public String index() {
+    public String index(){
         return "index";
     }
     @GetMapping("/code/SymmetricTree")
